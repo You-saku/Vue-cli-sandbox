a1093cc18951f4b20096ebb6eccc8d5979a938ba
You-saku <yousaku.dev19@gmail.com>
2021-08-01 00:01:04 +0900

    Additional(20210731)

--- app/sample_vue_app/src/App.vue
+++ app/sample_vue_app/src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <Sample1/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import Sample1 from './components/Sample1.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Sample1
   }
 }
 </script>
---
+++ app/sample_vue_app/src/components/Sample1.vue
@@ -0,0 +1,67 @@
+<template>
+    <h1>Shopping</h1>
+    <h2>Counter</h2>
+    <p>Count : {{ counter }}</p>
+    <span v-if="seen">You can see!!</span>
+    <button v-on:click="counter+=1">Add</button>
+    <button v-on:click="sub">Sub</button>
+    <button v-on:click="counter=0">Reset</button>
+
+    <h2>Item</h2>
+    <p>Price : {{price}} ¥</p>
+
+    <h2>Total Amount</h2>
+    <p>Total Amount : {{totalPrice}} ¥</p>
+
+    <h1>I lived those prefectures.</h1>
+    <ul>
+    <li v-for="prefecture in prefectures" :key="prefecture.item">
+        {{ prefecture.item }}
+    </li>
+    </ul>
+
+</template>
+
+<script>
+export default {
+    name: 'Sample1',
+    data(){
+        return{
+            counter: 0,
+            price: 100,
+            seen: false,
+            prefectures: [
+                {item: 'Gunmma'},
+                {item: 'Saitama'},
+                {item: 'Tokyo'},
+            ],
+        }
+    },
+
+    methods: {
+        sub: function(){
+            this.counter--;
+            if(this.counter < 0){
+                this.counter = 0;
+            }
+        }
+    },
+    
+    computed: {
+        totalPrice: function(){
+            if(this.counter < 0){
+                return 0;
+            }else{
+                return this.counter * this.price;
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    ul {
+        list-style-type:none;
+        display: inline;
+    }
+</style>
